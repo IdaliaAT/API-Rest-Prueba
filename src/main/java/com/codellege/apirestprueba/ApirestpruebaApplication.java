6d4e35f187411d361407b7ced25f02ed53773d2e
Idalia Aranda <nidaliaamtz@hotmail.com>
2023-08-05 19:14:37 -0500

    First Commit

---
+++ src/main/java/com/codellege/apirestprueba/ApirestpruebaApplication.java
@@ -0,0 +1,13 @@
+package com.codellege.apirestprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestpruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestpruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codellege/apirestprueba/ApirestpruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codellege.apirestprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestpruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
